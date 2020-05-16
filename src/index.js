9d24460d69260acb021dca37bd7afabe36445423
Irlan <irlan.freitas@hotmail.com>
2020-05-15 21:48:59 -0300

    Limitando a quantidade de recursos consumidos pelo node

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from '~/views/app/App';
-import store from '~/config/store';
+import App from './views/app/App';
+import store from './config/store';
 
 import 'antd/dist/antd.css';
-import '~/assets/style/global.scss';
+import './assets/style/global.scss';
 
 ReactDOM.render(
   <Provider store={store}>
